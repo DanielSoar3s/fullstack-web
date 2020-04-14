4b475705ed790d847028c803137d5bb3a077543f
Daniel Soares <danielsoaaraujo@hotmail.com>
2020-04-14 12:20:15 -0300

    update

--- gulp/spa/gulpTasks/deps.js
+++ gulp/spa/gulpTasks/deps.js
@@ -1,13 +1,18 @@
 const gulp = require('gulp');
+const unglifycss = require('gulp-uglifycss');
+const concat = require('gulp-concat');
 
-
-function depsCSS( cb ) {
-    return cb();
+function depsCSS() {
+    return gulp.src('node_modules/font-awesome/css/font-awesome.css')
+        .pipe(unglifycss({ "uglyComments": false }))
+        .pipe(concat('deps.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 }
 
 
-function depsFonts( cb ) {
-    return cb();
+function depsFonts() {
+    return gulp.src('node_modules/font-awesome/css/fonts/*.*')
+        .pipe(gulp.dest('build/assets/fonts'))
 }
 
 module.exports = {
