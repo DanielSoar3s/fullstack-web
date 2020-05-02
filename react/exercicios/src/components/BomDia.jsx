e90c746e7eafd2f55b2cceee27407b8e9bf5780f
Daniel Soares <danielsoaaraujo@hotmail.com>
2020-05-02 18:42:04 -0300

    update

---
+++ react/exercicios/src/components/BomDia.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+export default props => [
+    <h1 key="h1">Bom dia, {props.nome}</h1>,
+    <h2 key="h2">Valeu</h2>
+]
+
+
+
+// export default props => 
+//     <React.Fragment>
+//         <h1>Bom dia {props.nome}</h1>
+//         <h2>Volte Sempre em nossa loja.</h2>
+//     </React.Fragment>
+
+
+
+
+
+// export default props => 
+//     <Fragment>
+//         <h1>Bom dia {props.nome}</h1>
+//         <h2>Volte Sempre em nossa loja.</h2>
+//     </Fragment>
---
+++ react/exercicios/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+import BomDia from './components/BomDia';
+
+ReactDom.render( <BomDia nome="Daniel Soares"/>, document.getElementById('root'));
